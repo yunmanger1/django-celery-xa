90d8bbdb191207cff8a3ac35d444850c701b310a
German Ilyin <germanilyin@gmail.com>
2011-11-22 12:02:56 +0600

    consider transaction management

--- celery_xa/patcher.py
+++ celery_xa/patcher.py
@@ -2,12 +2,13 @@
 from django.conf import settings
 from .utils.queue import store
 from django.utils.functional import curry
+from django.db import transaction
 
 def wrap_func(func):
     old_func = func
     def new_func(*args, **kwargs):        
-        if settings.BROKER_TRANSPORT == "django":
-            # если используется db транспорт, то нам это не нужно.
+        if settings.BROKER_TRANSPORT == "django" or not transaction.is_managed():
+            # если используется db транспорт или мы не под транзакцией, то нам это не нужно.
             # все работает по старому 
             return old_func(*args, **kwargs)
         else:
