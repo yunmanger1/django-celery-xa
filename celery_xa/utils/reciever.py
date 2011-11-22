f911eb18aea897555da240ba709c426b0fb569cc
German Ilyin <germanilyin@gmail.com>
2011-11-22 12:22:22 +0600

    clean logging

--- celery_xa/utils/reciever.py
+++ celery_xa/utils/reciever.py
@@ -1,17 +1,12 @@
 from django.db import transaction
 from django.dispatch.dispatcher import receiver
 from .queue import store
-import logging
-log = logging.getLogger('DEFAULT')
 
 @receiver(transaction.signals.post_commit) #@UndefinedVariable
 def after_commit(*args, **kwargs):
-    
     for obj in store.queue:
         if callable(obj):
             obj()
-            print obj
-            log.debug('asdasd')
     store.clear()
     if transaction.is_dirty():
         transaction.commit()
